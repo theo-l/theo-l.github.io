193b4bd92d5bac34d94d875fbca0b9eaf593798f
theo-l <theol.liang@gmail.com>
2020-02-03 18:04:01 -0300

    stage iteration changes

--- hacking_python_iteration_python_source/1_iteration_intro_demo.py
+++ hacking_python_iteration_python_source/1_iteration_intro_demo.py
@@ -1,69 +1,64 @@
-from copy import copy
 class Pizza:
     def __init__(self, data, muiltple=True):
         print(f'\nCreated an Iterable object <{type(data).__name__.upper()} pizza>!\n')
         self.data = data
-        self.muiltple=muiltple
+        self.muiltple = muiltple
 
     def __iter__(self):
+        from copy import copy
         print('<Using iteration protocol method!>\n')
         print('<waiter serve>:', end='')
         self.ix = 0
-        if self.muiltple:
-            return copy(self)
+        if self.muiltple: return copy(self)
         return self
 
     def __next__(self):
         print('<next piece>:', end='')
-        if self.ix >= len(self.data):
-            raise StopIteration('Pizza is finished')
+        if self.ix >= len(self.data): raise StopIteration('Pizza is finished')
         result = self.data[self.ix]
         self.ix += 1
         return result
 
 
 class Waiter:
-
-    def __init__(self, pizza:Pizza):
+    def __init__(self, pizza: Pizza):
         print('Trying to convert an Iterable object to Iterator!\n')
         self.pizza = iter(pizza)
 
     def next(self):
         return next(self.pizza)
-    
+
+
 class Pizzaeria:
-    
     @classmethod
     def make_list_pizza(cls):
-        pizza = Pizza([1,2,3,4,5])
+        pizza = Pizza([1, 2, 3, 4, 5])
         return cls.serve(pizza)
 
     @classmethod
     def make_tuple_pizza(cls):
-        pizza = Pizza((1,2,3,4,5,6))
+        pizza = Pizza((1, 2, 3, 4, 5, 6))
         return cls.serve(pizza)
-
 
     @classmethod
     def serve(cls, pizza):
         print('Calling in Iteration context <PIZZAERIA>!\n')
         return Waiter(pizza)
 
+
 class Client:
-    
-    @staticmethod
-    def order_pizza(pizza_type):
+    def order_pizza(self, pizza_type):
         return pizza_type
 
-    @staticmethod
-    def ask_pizza(waiter:Waiter):
+    def ask_pizza(self, waiter: Waiter):
         return waiter.next()
 
 
 if __name__ == "__main__":
+    print(f'\n{"="*40}')
     liang = Client()
     pizza_type = liang.order_pizza('list')
-    pizza = Pizzaeria.make_list_pizza() # call directly
+    pizza = Pizzaeria.make_list_pizza()  # call directly
     try:
         liang.ask_pizza(pizza)
         liang.ask_pizza(pizza)
@@ -74,11 +69,3 @@ if __name__ == "__main__":
         liang.ask_pizza(pizza)
     except StopIteration as e:
         print(f'\n{"="*40}\n{e}\n')
-
-
-
-
-
-
-
-
