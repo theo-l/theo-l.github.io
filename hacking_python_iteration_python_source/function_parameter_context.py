3d2ad65061ffa382aa182389a92b72919cf53c6d
theo-l <theol.liang@gmail.com>
2020-02-04 00:35:42 -0300

    stage changes

--- hacking_python_iteration_python_source/function_parameter_context.py
+++
@@ -1,5 +0,0 @@
-from base_demo import Iterable
-
-if __name__ == "__main__":
-    iterable_object = Iterable([1,2,3,4,6,5,3])
-    print('\n', '<FUNCTION parameter> iteration context', *iterable_object, f'\n{"-"*50}\n')
--- hacking_python_iteration_python_source/list_comprehensions_demo1.py
+++ hacking_python_iteration_python_source/list_comprehensions_demo1.py
@@ -1,7 +1,26 @@
-def main():
-    a_list = [i for i in range(10)]
-    print(a_list)
-
-
 if __name__ == "__main__":
-    main()
+    string = 'hello, python list comprehensions'
+
+    print([ord(c.upper()) for c in string])
+    print({ord(c.upper()) for c in string})
+    print({c.upper():ord(c.upper()) for c in string})
+    print((ord(c.upper()) for c in string)) # Generator
+
+    print('\n', '-'*60, '\n')
+
+    # Normal for loop
+    list_ords = []
+    for c in string:
+        list_ords.append(ord(c.upper()))
+    print(list_ords)
+
+
+    set_ords = set()
+    for c in string:
+        set_ords.add(ord(c.upper()))
+    print(set_ords)
+
+    dict_ords = {}
+    for c in string:
+        dict_ords.update({c.upper():ord(c.upper())})
+    print(dict_ords)
